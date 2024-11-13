0af1524f4f6a0bf5b013ba2b3e626465ad193fa1
Peter Jung <peter@jung.ninja>
2024-11-13 13:34:23 +0700

    Add link to finished agents to readme

--- main.py
+++ main.py
@@ -10,4 +10,6 @@ load_dotenv()
 class MyAgent(prediction_market_agent_tooling.deploy.agent.DeployableTraderAgent):
     """
     TODO: Implement market betting agent.
+
+    Step-by-step implementations can be found at https://github.com/gnosis/gnosis-ai-hackathon-starter/tree/step-4/trader_quickstart/agents.
     """
